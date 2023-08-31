6e93165cca17722ef8bc8fd9479691d31664c35b
Jackson Ramos <jackson.ramoos@outlook.com>
2023-08-30 23:34:48 -0300

    ajustes em diretórios

--- v1-MVC/servico/aluno_service.js
+++ v1-MVC/servico/aluno_service.js
@@ -8,6 +8,9 @@ class AlunoService {
         if (alunoPesquisado.length > 0) {
             throw new Error('Aluno já cadastrado!');
         }
+        else if (idade < 18) {
+            throw new Error('Aluno menor de idade!');
+        }
         const alunoNovo = new Aluno(nome, idade, matricula);
         this.repositorio.inserir(alunoNovo);
         return alunoNovo;
